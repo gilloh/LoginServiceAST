d1fa5bbf1541aecf9adc16a0416c36bdc5c737cd
Bruno <bgbrodrigues@gmail.com>
2019-01-10 10:19:14 +0100

    Added routes Added login and home page

---
+++ login_service_ast/lib/login_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatefulWidget {
+  static String tag = 'login-page';
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      Text('Login');    
+    );
+  }
+}
